3b58cc77f42e533cb13615812b0fff8263f7b9e3
Gytis Barkauskas <gb.korumuda@gmail.com>
2017-12-04 21:51:00 +0200

    Routing setup, gallery and pagenotfount modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CartComponent } from './cart/cart.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { PageNotFoundModule } from './page-not-found/page-not-found.module';
+import { GalleryModule } from './gallery/gallery.module';
 
+const appRoutes: Routes = [
+  { path: 'gallery', component: GalleryComponent},
+  { path: 'cart', component: CartComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    CartComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    PageNotFoundModule,
+    GalleryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
