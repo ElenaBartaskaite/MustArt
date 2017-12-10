57943ac90fb3263e6d87750b7e13a4965ffdd86c
Gytis Barkauskas <gb.korumuda@gmail.com>
2017-12-10 17:53:06 +0200

    Fixed gallery.component mess after merge

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -15,16 +15,8 @@ import { Router } from '@angular/router';
   providers: [SearchService, ImageService]
 })
 export class GalleryComponent implements OnInit {
-  images: SearchImage[];
-    { "id": "1", "name": "name1", "description": "name1", "matchingColors": [new Color(0,0,0)], "dominantColor": new Color(0,0,0), "uploadDate": new Date(2017, 1, 1), "tags": ["a", "basd", "aasdf", "basd", "aasdf", "basd", "aasdf"], "relevance": 2},
-    { "id": "2", "name": "name2", "description": "name2", "matchingColors": [new Color(0,0,0)], "dominantColor": new Color(0,0,0), "uploadDate": new Date(2017, 1, 3), "tags": ["a", "b"], "relevance": 1},
-    { "id": "3", "name": "name3", "description": "name3", "matchingColors": [new Color(0,0,0)], "dominantColor": new Color(0,0,0), "uploadDate": new Date(2017, 1, 2), "tags": ["a", "b"], "relevance": 0},
-    { "id": "4", "name": "name4", "description": "name4", "matchingColors": [new Color(0,0,0)], "dominantColor": new Color(0,0,0), "uploadDate": new Date(2017, 1, 4), "tags": ["a", "b"], "relevance": 1},
-    { "id": "5", "name": "name5", "description": "name5", "matchingColors": [new Color(0,0,0)], "dominantColor": new Color(0,0,0), "uploadDate": new Date(2017, 1, 1), "tags": ["a", "b"], "relevance": 0},
-    { "id": "5", "name": "name6", "description": "name6", "matchingColors": [new Color(0,0,0)], "dominantColor": new Color(0,0,0), "uploadDate": new Date(2017, 1, 2), "tags": ["a", "b"], "relevance": 3},
-  
-  constructor(private searchService: SearchService, private router: Router) {
-  constructor(private searchService: SearchService, private imageService: ImageService) {
+  images: SearchImage[]; 
+  constructor(private searchService: SearchService, private router: Router, private imageService: ImageService) {
   }
 
   ngOnInit() {
