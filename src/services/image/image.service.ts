a2240447d16e8513a402871b48d23081d7aa4b9e
elenabart <elena.bartaskaite@gmail.com>
2017-12-19 23:04:54 +0200

    Real page imitation

--- src/services/image/image.service.ts
+++ src/services/image/image.service.ts
@@ -8,74 +8,97 @@ import { Color } from '../../models/color';
 export class ImageService {
   images: SearchImage[] = [
     {
-      "id": "1", "name": "name1", "description": "lorem",
+      "id": "1", "name": "Acid Chess", "description": "lorem",
       "matchingColors": [new Color(255, 0, 0)], "dominantColor": new Color(0, 0, 0),
-      "uploadDate": new Date(2017, 1, 1), "tags": ["a"], "relevance": 0,
+      "uploadDate": new Date(2017, 1, 1), "tags": ["chess","math","spirals"], "relevance": 0,
       "prices": { "mug": 10, "shirt": 15, "print": 10 },
       "questions": [{
-        "question": "?", "username": "user1", "askDate": new Date(2017, 1, 1), "verified": true,
+        "question": "What is this?", "username": "SomeGuy", "askDate": new Date(2017, 1, 1), "verified": true,
         "answers": [
-          { "answer": "yes", "relevance": 0, "username": "user2", "answerDate": new Date(2017, 1, 1), "best": true },
-          { "answer": "yes", "relevance": 0, "username": "user2", "answerDate": new Date(2017, 1, 1), "best": false },
-          { "answer": "yes", "relevance": 0, "username": "user2", "answerDate": new Date(2017, 1, 1), "best": false },
+          { "answer": "Idk", "relevance": 0, "username": "SomeUser", "answerDate": new Date(2017, 1, 1), "best": true },
+          { "answer": "yes", "relevance": 0, "username": "user69", "answerDate": new Date(2017, 1, 1), "best": false },
+          { "answer": "yes", "relevance": 0, "username": "user", "answerDate": new Date(2017, 1, 1), "best": false },
         ]
       }],
-      "reviews": [{ "text": "very good", "verified": true, "username": "user1", "reviewDate": new Date(2017, 1, 1) }]
+      "reviews": [{ "text": "Nice", "verified": true, "username": "user1", "reviewDate": new Date(2017, 1, 1) }]
     },
     {
-      "id": "2", "name": "name2", "description": "a b",
+      "id": "2", "name": "Look at Me", "description": "a b",
       "matchingColors": [new Color(0, 255, 0)], "dominantColor": new Color(0, 0, 0),
-      "uploadDate": new Date(2017, 1, 3), "tags": ["a"], "relevance": 0,
+      "uploadDate": new Date(2017, 1, 3), "tags": ["math","spirals"], "relevance": 0,
       "prices": { "mug": 10, "shirt": 15, "print": 10 },
       "questions": [{
-        "question": "?", "username": "user1", "askDate": new Date(2017, 1, 1), "verified": true,
+        "question": "Why?", "username": "user1", "askDate": new Date(2017, 1, 1), "verified": true,
+        "answers": [{ "answer": "no", "relevance": 0, "username": "user2", "answerDate": new Date(2017, 1, 1), "best": true }]
+      }],
+      "reviews": [{ "text": "very good", "verified": true, "username": "user1", "reviewDate": new Date(2017, 1, 1) }]
+    },
+    {
+      "id": "3", "name": "Zebra", "description": "lorem",
+      "matchingColors": [new Color(0, 0, 0)], "dominantColor": new Color(0, 0, 0),
+      "uploadDate": new Date(2017, 1, 2), "tags": ["lines","math"], "relevance": 0,
+      "prices": { "mug": 10, "shirt": 15, "print": 10 },
+      "questions": [{
+        "question": "How to use this??", "username": "user1", "askDate": new Date(2017, 1, 1), "verified": true,
         "answers": [{ "answer": "yes", "relevance": 0, "username": "user2", "answerDate": new Date(2017, 1, 1), "best": true }]
       }],
       "reviews": [{ "text": "very good", "verified": true, "username": "user1", "reviewDate": new Date(2017, 1, 1) }]
     },
     {
-      "id": "3", "name": "name3", "description": "lorem",
+      "id": "4", "name": "Home", "description": "name1",
       "matchingColors": [new Color(0, 0, 0)], "dominantColor": new Color(0, 0, 0),
-      "uploadDate": new Date(2017, 1, 2), "tags": ["b"], "relevance": 0,
+      "uploadDate": new Date(2017, 1, 4), "tags": ["color","math", "noise"], "relevance": 0,
       "prices": { "mug": 10, "shirt": 15, "print": 10 },
       "questions": [{
-        "question": "?", "username": "user1", "askDate": new Date(2017, 1, 1), "verified": true,
+        "question": "I broke my cup. Can I get a refund?", "username": "user1", "askDate": new Date(2017, 1, 1), "verified": true,
+        "answers": [{ "answer": "no", "relevance": 0, "username": "user2", "answerDate": new Date(2017, 1, 1), "best": true }]
+      }],
+      "reviews": [{ "text": "very good", "verified": true, "username": "user1", "reviewDate": new Date(2017, 1, 1) }]
+    },
+    {
+      "id": "5", "name": "Wooden Jokes", "description": "ipsum",
+      "matchingColors": [new Color(0, 0, 0)], "dominantColor": new Color(223, 191, 159),
+      "uploadDate": new Date(2017, 1, 1), "tags": ["noise", "math"], "relevance": 0,
+      "prices": { "mug": 10, "shirt": 15, "print": 10 },
+      "questions": [{
+        "question": "Why?", "username": "user1", "askDate": new Date(2017, 1, 1), "verified": true,
         "answers": [{ "answer": "yes", "relevance": 0, "username": "user2", "answerDate": new Date(2017, 1, 1), "best": true }]
       }],
       "reviews": [{ "text": "very good", "verified": true, "username": "user1", "reviewDate": new Date(2017, 1, 1) }]
     },
     {
-      "id": "4", "name": "name4", "description": "name1",
-      "matchingColors": [new Color(0, 0, 0)], "dominantColor": new Color(0, 0, 0),
-      "uploadDate": new Date(2017, 1, 4), "tags": ["b"], "relevance": 0,
+      "id": "6", "name": "Dark Trip", "description": "rrr",
+      "matchingColors": [new Color(0, 0, 0)], "dominantColor": new Color(255, 0, 0),
+      "uploadDate": new Date(2017, 1, 2), "tags": ["color", "spirals", "math"], "relevance": 0,
       "prices": { "mug": 10, "shirt": 15, "print": 10 },
       "questions": [{
-        "question": "?", "username": "user1", "askDate": new Date(2017, 1, 1), "verified": true,
+        "question": "Who is the artist?", "username": "user1", "askDate": new Date(2017, 1, 1), "verified": true,
         "answers": [{ "answer": "yes", "relevance": 0, "username": "user2", "answerDate": new Date(2017, 1, 1), "best": true }]
       }],
       "reviews": [{ "text": "very good", "verified": true, "username": "user1", "reviewDate": new Date(2017, 1, 1) }]
     },
     {
-      "id": "5", "name": "name5", "description": "ipsum",
-      "matchingColors": [new Color(0, 0, 0)], "dominantColor": new Color(0, 0, 0),
-      "uploadDate": new Date(2017, 1, 1), "tags": ["a", "b"], "relevance": 0,
+      "id": "7", "name": "Noisy World", "description": "rrr",
+      "matchingColors": [new Color(0, 0, 0)], "dominantColor": new Color(51, 204, 51),
+      "uploadDate": new Date(2017, 1, 2), "tags": ["color", "spirals", "math"], "relevance": 0,
       "prices": { "mug": 10, "shirt": 15, "print": 10 },
       "questions": [{
-        "question": "?", "username": "user1", "askDate": new Date(2017, 1, 1), "verified": true,
+        "question": "What the f?", "username": "user1", "askDate": new Date(2017, 1, 1), "verified": true,
         "answers": [{ "answer": "yes", "relevance": 0, "username": "user2", "answerDate": new Date(2017, 1, 1), "best": true }]
       }],
       "reviews": [{ "text": "very good", "verified": true, "username": "user1", "reviewDate": new Date(2017, 1, 1) }]
     },
     {
-      "id": "6", "name": "name6", "description": "rrr",
-      "matchingColors": [new Color(0, 0, 0)], "dominantColor": new Color(0, 0, 0),
-      "uploadDate": new Date(2017, 1, 2), "tags": ["a", "b"], "relevance": 0,
+      "id": "8", "name": "Drops", "description": "rrr",
+      "matchingColors": [new Color(0, 0, 0)], "dominantColor": new Color(51, 204, 51),
+      "uploadDate": new Date(2017, 1, 2), "tags": ["color", "spirals", "math"], "relevance": 0,
       "prices": { "mug": 10, "shirt": 15, "print": 10 },
       "questions": [{
-        "question": "?", "username": "user1", "askDate": new Date(2017, 1, 1), "verified": true,
+        "question": "Where is this?", "username": "user1", "askDate": new Date(2017, 1, 1), "verified": true,
         "answers": [{ "answer": "yes", "relevance": 0, "username": "user2", "answerDate": new Date(2017, 1, 1), "best": true }]
       }],
-      "reviews": [{ "text": "very good", "verified": true, "username": "user1", "reviewDate": new Date(2017, 1, 1) }]
+      "reviews": [{ "text": "very good", "verified": true, "username": "userOne", "reviewDate": new Date(2017, 1, 1) },
+      { "text": "I don't like this", "verified": false, "username": "Otheruser", "reviewDate": new Date(2017, 1, 1) }]
     },
   ];
   getImages(): SearchImage[] {
