b16de69406bd82b268d1c29c7ef9f763aed7512b
Gytis Barkauskas <gb.korumuda@gmail.com>
2017-12-19 22:30:29 +0200

    Upload page

--- src/services/notification/notification.service.ts
+++ src/services/notification/notification.service.ts
@@ -5,18 +5,25 @@ import { ToastrService } from 'ngx-toastr';
 @Injectable()
 export class NotificationService {
 
+  params = {
+    closeButton: true,
+    timeOut: 1500,
+    progressBar: true,
+    positionClass: 'toast-bottom-right'
+  }
+
   constructor(private toastr: ToastrService) { }
 
   showSuccess(title: string, message: string) {
-    this.toastr.success(message, title);
+    this.toastr.success(message, title, this.params);
   }
 
   showError(title: string, message: string) {
-    this.toastr.error(message, title)
+    this.toastr.error(message, title, this.params)
   }
 
   showInfo(title: string, message: string) {
-    this.toastr.info(message, title);
+    this.toastr.info(message, title, this.params);
   }
 
 }
