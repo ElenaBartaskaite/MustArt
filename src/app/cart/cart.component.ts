6d29c788600a137152a716a506ee793c30a0eb65
elenabart <elena.bartaskaite@gmail.com>
2017-12-19 22:17:54 +0200

    cart remove, cart price

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -13,7 +13,7 @@ import { ImageService } from '../../services/image/image.service';
 export class CartComponent implements OnInit {
 
   cartItems: any[];
-  
+
 
   constructor(
     private imageService: ImageService
@@ -24,5 +24,19 @@ export class CartComponent implements OnInit {
     this.cartItems = CartService.cartItems;
     console.log(this.cartItems);
   }
-
+  Remove(item){
+    CartService.removeItem(item.id, item.product, item.details)
+  }
+  GetTotal():number{
+    var total:number=0;
+    for (let item of this.cartItems) {
+      total=total+ this.getPrice(item);
+    }
+    return total;
+  }
+  getPrice(item):number{
+    if (item.product=="Mug")return this.imageService.getImageById(item.id).prices.mug;
+    else if (item.product=="Print")return this.imageService.getImageById(item.id).prices.print;
+    else if (item.product=="Tshirt")return this.imageService.getImageById(item.id).prices.shirt;
+  }
 }
